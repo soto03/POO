18a1e6ab729d2afaede42304717a8b83cf0ee430
Soto03 <diegosoto@liceovvh.cl>
2024-03-15 10:51:19 -0300

    ok

--- Fundamentos/01_Ejercicios/script.js
+++ Fundamentos/01_Ejercicios/script.js
@@ -4,4 +4,6 @@ const constante = "Constante";
 
 console.log("variables: Global: " + global + "- Local: " + local + "- Constante: " + constante);
 
-
+var numero = 33;
+var numeroDecimal = 3.4;
+var texto = "hola causas";
