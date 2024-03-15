731924a5afce8e3ed3f9f7791447cb49d638e8b2
Soto03 <diegosoto@liceovvh.cl>
2024-03-15 11:10:34 -0300

    ok

--- Fundamentos/01_Ejercicios/script.js
+++ Fundamentos/01_Ejercicios/script.js
@@ -1,15 +1,37 @@
+
+//Variable globlal
 var global = "Global";
+//Variable local
 let local = "local";
+//Variable constante
 const constante = "Constante";
 
 console.log("variables: Global: " + global + "- Local: " + local + "- Constante: " + constante);
 
-let numero = 33;let numeroDecimal = 3.4;
+//Variable de numero entero
+let numero = 33;
+
+//Variable de numero decimal
+let numeroDecimal = 3.4;
+
+//Variable de cadena de texto
 let texto = "hola causas";
+
+//Variable de booleano
 let registroDeCompra = true;
+
+//Variable de arreglo
 let Semana = ["lunes","martes","miercoles","jueves","viernes"];
+
+//Variable de objeto
 let cosa = {messi:10};
+
+//Variable indefinida
 let indefinida;
+
+//Variable de tipo null
 let nulo = null;
+
+//Una Funcion
 function suma() {return 5 + 5
 };
