13d53a0f19b6f290633489b1166f8ce1b15f024c
Soto03 <diegosoto@liceovvh.cl>
2024-03-18 11:14:31 -0300

    ok

--- Fundamentos/02_Ejercicios/script.js
+++ Fundamentos/02_Ejercicios/script.js
@@ -3,19 +3,19 @@
 // Instrucciones: Declara dos variables numéricas llamadas 'actual' y 'nac'.
 // Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
 function declaracionVariables(){
-
+let actual = 2024;
+let nac = 2007
+let resultado = actual - nac;
+alert ("mi edad es " + resultado);
 }
-
-// Tu código aquí
-
 
 // Ejercicio 2: Asignación de valores
-// Instrucciones: Completa la función para asignar el valor de 'x' a 'y' y luego muestra el valor de 'y'.
+// Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'.
 function asignaValores(){
-
+    let var1 = 33;
+    let var2 = var1;
+    alert("El valor de y es " + var2);
 }
-
-// Tu código aquí
 
 
 // Ejercicio 3: Operaciones matemáticas
@@ -33,7 +33,7 @@ function realizarOperaciones(){
 // Instrucciones: Completa la función para construir un mensaje utilizando las variables 'nombre' y 'apellido'.
 // Luego, muestra la longitud del mensaje y el primer carácter en el mensaje.
 function manipulaciónCadenas(){
-    
+
 }
 // Tu código aquí
 
