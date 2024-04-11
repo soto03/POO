768333dd6c0c880353efd7c5d6416e5ce036a931
soto03 <diegosoto@liceovvh.cl>
2024-04-11 14:57:50 -0400

    ok

--- Fundamentos/05_Bucles/04_Ejercicios/script.js
+++ Fundamentos/05_Bucles/04_Ejercicios/script.js
@@ -1,12 +1,12 @@
 function verificar(){
-    let num1 = prompt("Ingrese un numero")
-    let num2 = prompt("Ingrese un numero")
-    let num3 = prompt("Ingrese un numero")
-    let num4 = prompt("Ingrese un numero")
-    let num5 = prompt("Ingrese un numero")
+    let numeroMayor = parseFloat(prompt("Ingrese el número 1:"));
 
-    for(num1.num2,num3,num4,num5){
-
+for (let i = 2; i <= 5; i++) {
+    let numero = parseFloat(prompt("Ingrese el número " + i + ":"));
+    if (numero > numeroMayor) {
+        Mayor = numero;
     }
-    document.getElementById(`lista`).innerHTML = `<h2>Suma de Numeros Pares: <br>${array}</h2>`
 }
+    document.getElementById(`lista`).innerHTML = Mayor
+}
+
