e6749652cafc7781d1a449f53dbb5f8bbda8ca83
soto03 <diegosoto@liceovvh.cl>
2024-04-12 10:21:40 -0400

    ok

---
+++ Fundamentos/05_Bucles/05_Ejercicios/script.js
@@ -0,0 +1,11 @@
+function NnumerosPares(){
+    let N = parseInt(prompt("Ingrese un numero cualquiera"))
+    let count = 0;
+    let array = []
+
+    for(let i = 2; count < N; i +=2){
+        array.push(i)
+        count++;
+    }
+    document.getElementById(`lista`).innerHTML = `<h2>Estos son la cantidad de numeros pares encontrados: ${array}</h2>)`
+}
