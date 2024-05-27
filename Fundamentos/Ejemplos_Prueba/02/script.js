39ec789da77c2d7d4afc32c180718052101c6720
soto03 <diegosoto@liceovvh.cl>
2024-05-27 14:16:13 -0400

    ok

--- Fundamentos/Ejemplos_Prueba/02/script.js
+++ Fundamentos/Ejemplos_Prueba/02/script.js
@@ -1,1 +1,12 @@
-f
+function noNeg(){
+    let array = []
+    let N = parseInt(prompt("Ingresa cuántos números quiera:"));
+    for(let i = 0; i < N; i++){
+        let num = parseInt(prompt("Ingresa numeros positivos y negativos: "));
+        array.push(num)
+        if(array[i] + num){
+            array[i] + num
+        }
+    }
+    document.getElementById(`Elemento`).innerHTML = `Arreglo sumado: ${sum} <br> Arreglo modificado: ${array.join(",")}`
+};
