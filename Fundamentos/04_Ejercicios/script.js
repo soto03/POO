dcb730227692df28af9b923503fc41915a58b6fc
soto03 <diegosoto@liceovvh.cl>
2024-05-27 14:27:56 -0400

    ok

---
+++ Fundamentos/04_Ejercicios/script.js
@@ -0,0 +1,6 @@
+function Suma_indices() {
+    let arreglo = [1, 2, 3, 4, 5, 6, 7];
+    let suma = arreglo[6] + arreglo[5] + arreglo[0]; 
+    
+    alert("La suma de los valores en los índices 6, 5 y 0 es: " + suma);
+}
