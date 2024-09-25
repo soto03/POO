d0c21ac2a104e04a824292a527b91ab0a795f93d
soto03e <diegosoto@liceovvh.clm>
2024-09-25 10:53:12 -0300

    ok

---
+++ Fundamentos/2.1/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+function login(element){
+    if (element.innerText == "Login"){
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
